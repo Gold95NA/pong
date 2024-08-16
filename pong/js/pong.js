a037242d8a5babedf331aef59ad15f6cd18b0bf3
Gold95NA <156334092+Gold95NA@users.noreply.github.com>
2024-08-15 22:11:28 -0400

    commit

--- pong/js/pong.js
+++ pong/js/pong.js
@@ -37,22 +37,28 @@ var pad = [];
 pad[0] = player[0].pad;
 pad[1] = player[1].pad;
 
+var playerScores = document.querySelectorAll('.score');
+
 function main() {
     //erases the canvas
     ctx.clearRect(0, 0, c.width, c.height);
 
     //player movement
-    if (keys[`w`]) {
+    if (keys[`w`]) 
+    {
         pad[0].vy += -pad[0].force;
     }
-    if (keys[`s`]) {
+    if (keys[`s`]) 
+    {
         pad[0].vy += pad[0].force;
     }
 
-    if (keys[`ArrowUp`]) {
+    if (keys[`ArrowUp`]) 
+    {
         pad[1].vy += -pad[1].force;
     }
-    if (keys[`ArrowDown`]) {
+    if (keys[`ArrowDown`]) 
+    {
         pad[1].vy += pad[1].force;
     }
 
@@ -64,52 +70,62 @@ function main() {
     pad[0].move();
     pad[1].move();
 
-    if (pad[0].y < 0 + pad[0].h / 2) {
+    if (pad[0].y < 0 + pad[0].h / 2) 
+    {
         pad[0].y = 0 + pad[0].h / 2;
     }
-    if (pad[0].y > c.height - pad[0].h / 2) {
+    if (pad[0].y > c.height - pad[0].h / 2) 
+    {
         pad[0].y = c.height - pad[0].h / 2;
     }
 
-    if (pad[1].y < 0 + pad[1].h / 2) {
+    if (pad[1].y < 0 + pad[1].h / 2) 
+    {
         pad[1].y = 0 + pad[1].h / 2;
     }
-    if (pad[1].y > c.height - pad[1].h / 2) {
+    if (pad[1].y > c.height - pad[1].h / 2) 
+    {
         pad[1].y = c.height - pad[1].h / 2;
     }
 
     //ball movement
     ball.move();
 
-    if (ball.x < 0) {
+    if (ball.x < 0) 
+    {
         player[1].score++;
         ball.x = c.width / 2;
         ball.y = c.height / 2;
         ball.vx = -ball.vx;
     }
 
-    if (ball.x > c.width) {
+    if (ball.x > c.width) 
+    {
         player[0].score++;
         ball.x = c.width / 2;
         ball.y = c.height / 2;
         ball.vx = -ball.vx;
     }
 
-    if (ball.y < 0) {
+    if (ball.y < 0) 
+    {
         ball.y = 0;
         ball.vy = -ball.vy;
     }
-    if (ball.y > c.height) {
+    if (ball.y > c.height) 
+    {
         ball.y = c.height;
         ball.vy = -ball.vy;
     }
 
-    if (ball.collide(pad[0])) {
+    if (ball.collide(pad[0])) 
+    {
         ball.x = pad[0].x + pad[0].w / 2 + ball.w / 2;
         ball.vx = -ball.vx;
     }
 
-    if (ball.collide(pad[1])) {
+    if (ball.collide(pad[1])) 
+    {
         ball.x = pad[1].x - pad[1].w / 2 - ball.w / 2;
         ball.vx = -ball.vx;
     }
@@ -120,4 +136,9 @@ function main() {
     pad[0].draw();
     pad[1].draw();
     ball.draw();
+
+    for (let i = 0; i < playerScores.length; i++) 
+    {
+        playerScores[i].innerText = player[i].score;
+    }
 }
